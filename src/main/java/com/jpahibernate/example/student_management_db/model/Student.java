55c6df50193d3a3bcfc39e990a94b8c4bd228f5a
Chandan K M <chandanchintu16@gmail.com>
2025-08-28 21:59:24 +0530

    JPA + Hibernate + connecting to db

---
+++ src/main/java/com/jpahibernate/example/student_management_db/model/Student.java
@@ -0,0 +1,14 @@
+package com.jpahibernate.example.student_management_db.model;
+
+public class Student {
+
+    private int id;
+
+    private String name;
+
+    private String mobile;
+
+    private String grade;
+
+    private String gender;
+}
