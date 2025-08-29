89ace274ea26ae82735035389aec847bbc0b4ff0
Chandan K M <chandanchintu16@gmail.com>
2025-08-29 22:16:14 +0530

    Creating table + save and find APIs

--- src/main/java/com/jpahibernate/example/student_management_db/controller/StudentController.java
+++ src/main/java/com/jpahibernate/example/student_management_db/controller/StudentController.java
@@ -1,4 +1,39 @@
 package com.jpahibernate.example.student_management_db.controller;
 
+import com.jpahibernate.example.student_management_db.model.Student;
+import com.jpahibernate.example.student_management_db.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/student/apis")
 public class StudentController {
+
+    @Autowired
+    StudentService studentService;
+
+    // @RequestBody- it is used to take the inputs in the form of json objects(used for complete class)
+
+    // debugging - helps to track down the application line by line to get the complete flow of the application
+    @PostMapping("/save")
+    public String saveStudent(@RequestBody Student student){
+        String response = studentService.addStudent(student);
+        return response;
+    }
+
+    @GetMapping("/findAll")
+    public List<Student> findAllStudents(){
+        List<Student> studentList= studentService.getAllStudents();
+        return studentList;
+    }
+
+    // @PathVariable - input is taken in the url path
+    @GetMapping("/findById/{id}")
+    public Student findStudentById(@PathVariable int id){
+        Student student = studentService.getStudentById(id);
+        return student;
+    }
+
 }
--- src/main/java/com/jpahibernate/example/student_management_db/model/Student.java
+++ src/main/java/com/jpahibernate/example/student_management_db/model/Student.java
@@ -1,14 +1,78 @@
 package com.jpahibernate.example.student_management_db.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity // it represents that this class is entity or model class which is directly creating the database table or schema
+@Table(name="student") // it will create a table inside database
 public class Student {
 
+    @Id // it is the primary key of the table - it will be unique and it is not null
+    @Column(name="id") // it will create a column inside database table
     private int id;
 
+    @Column(name = "student_name", nullable = false) // nullable = false - it cannot contain null value
     private String name;
 
-    private String mobile;
+    @Column(name = "email", nullable = false, unique = true) // unique=true - it cannot contain duplicated value
+    private String email;
 
+    @Column(name = "grade", nullable = false)
     private String grade;
 
+    @Column(name = "gender", nullable = false)
     private String gender;
+
+    @Column(name="mobile", nullable = false)
+    private String mobile; //+91-7828888888
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
 }
